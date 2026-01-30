96cec2db44a69508e27a6d97703c3290214c18bc
ankanb84 <113242090+ankanb84@users.noreply.github.com>
2026-01-30 14:32:32 +0530

    Update Formspree endpoint to new ID

--- script.js
+++ script.js
@@ -220,7 +220,8 @@ if (contactForm) {
         
         try {
             // Send to Formspree (free backend service)
-            const response = await fetch('https://formspree.io/f/your-form-id', {
+            const response = await fetch('https://formspree.io/f/mjgrepal', {
+
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
